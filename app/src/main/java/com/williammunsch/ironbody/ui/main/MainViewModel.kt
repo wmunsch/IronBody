af5e70611d0905d42b54b7b9d2a94af792860de4
William Munsch <munschwi@gmail.com>
2020-12-30 18:26:34 -0500

    Design the layout for the workout fragment

--- app/src/main/java/com/williammunsch/ironbody/ui/main/MainViewModel.kt
+++ app/src/main/java/com/williammunsch/ironbody/ui/main/MainViewModel.kt
@@ -14,9 +14,17 @@ class MainViewModel(private val workoutRepository: WorkoutRepository) : ViewMode
     val codename: LiveData<Int>
         get() = _startNewVisibility
 
+    private val _workoutButtonLayoutVisibility = MutableLiveData<Int>()
+    val workoutButtonLayoutVisibility: LiveData<Int>
+        get() = _workoutButtonLayoutVisibility
+    private val _newLiftButtonVisibility = MutableLiveData<Int>()
+    val liftButtonVisibility: LiveData<Int>
+        get() = _newLiftButtonVisibility
+
     init {
         _startNewVisibility.value = View.VISIBLE
-
+        _workoutButtonLayoutVisibility.value = View.INVISIBLE
+        _newLiftButtonVisibility.value = View.INVISIBLE
         //val result = workoutRepository.userName
         //userName = Transformations.map(result) { result -> result.value }
 
@@ -24,10 +32,16 @@ class MainViewModel(private val workoutRepository: WorkoutRepository) : ViewMode
 
     fun startNewWorkout(){
         _startNewVisibility.value = View.GONE
+        _newLiftButtonVisibility.value = View.VISIBLE
     }
 
     fun startNewCardio(){
         _startNewVisibility.value = View.GONE
+        _newLiftButtonVisibility.value = View.VISIBLE
+    }
+
+    fun addLift(){
+        _workoutButtonLayoutVisibility.value = View.VISIBLE
     }
 
 
